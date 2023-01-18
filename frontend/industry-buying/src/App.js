8727685d0838cc9791c3afba8b13717de0e40ea7
PranayJadhao <pranayjadhao95@gmail.com>
2023-01-18 22:09:28 +0530

    adding day-2

--- frontend/industry-buying/src/App.js
+++ frontend/industry-buying/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Cart } from './Pages/Cart';
+import { AllRoutes } from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Cart/>
+   <AllRoutes/>
     </div>
   );
 }
