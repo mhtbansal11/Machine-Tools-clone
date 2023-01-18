4be0257aaa68bb99f4c018f90811a25895eeab75
Kishan0431 <Kishanvjoshi1998@gmail.com>
2023-01-18 22:42:26 +0530

    sidebar_homepage

--- frontend/industry-buying/src/App.js
+++ frontend/industry-buying/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Cart } from './Pages/Cart';
+import Sidebar1_H from './Components/Sidebar1_H';
 
 function App() {
   return (
     <div className="App">
-      <Cart/>
+      <Sidebar1_H />
     </div>
   );
 }
