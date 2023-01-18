e76044f7e23c93af096802da9f24cf07ef4098da
PranayJadhao <pranayjadhao95@gmail.com>
2023-01-18 23:15:33 +0530

    adding day-2

--- frontend/industry-buying/src/Pages/ProductPage.jsx
+++ frontend/industry-buying/src/Pages/ProductPage.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { addProductData, getProductData } from '../Redux/AppReducer/action'
+import { useDispatch, useSelector } from "react-redux"
 
 const ProductPage = () => {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getProductData())
+        dispatch(addProductData())
+    },[])
+
   return (
-    <div>ProductPage</div>
+    // parent div
+    <div> 
+       
+        {/* div for heading */}
+        <div>
+            <h1>ProductPage</h1>
+        </div>
+           
+            {/* div for page */}
+            <div style={{'display':"flex"}}>
+                    {/* div for side bar */}
+                    <div style={{"width":"30%", "border":"1px solid red", "height":"100vh"}}></div>
+                
+                
+                    {/* div for products */}
+                    <div style={{"width":"70%", "border":"5px solid green", "height":"100vh"}}>
+                        
+                    </div>
+            </div>
+            
+    </div>
   )
 }
 
